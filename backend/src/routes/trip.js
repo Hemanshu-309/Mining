6358ae863feb11ac173d40fbb6150da3f8cae739
Rahul-307 <146062215+Rahul-307@users.noreply.github.com>
2023-11-20 12:12:07 +0530

    remove add remove and update from trip

--- backend/src/routes/trip.js
+++ backend/src/routes/trip.js
@@ -4,11 +4,11 @@ import middleware from '../middleware/jwt.js'
 
 const router = express.Router()
 
-router.post('/addTripType',middleware.checkJwt,trip.createTrip)
+//router.post('/addTripType',middleware.checkJwt,trip.createTrip)
 router.post('/getTripType',middleware.checkJwt,trip.getTrip)
-router.post('/deleteTripType',middleware.checkJwt,trip.deleteTrip)
-router.post('/deleteMultipleTripType',middleware.checkJwt,trip.deletedMultipleTrip)
-router.post('/updateTripType',middleware.checkJwt,trip.updateTrip)
+//router.post('/deleteTripType',middleware.checkJwt,trip.deleteTrip)
+//router.post('/deleteMultipleTripType',middleware.checkJwt,trip.deletedMultipleTrip)
+//router.post('/updateTripType',middleware.checkJwt,trip.updateTrip)
 router.post('/paginateTripType',middleware.checkJwt,trip.paginateTrip)
 
 export default {
